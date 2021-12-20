03ed7fc05e26f1f016bb2409d1a27a1a6e2225fd
Slava <webkey@mail.ru>
2021-12-20 21:15:30 +0300

    fixed: [WEB] Selectables rectSelect zoom issue;

--- web/static/multiselect.js
+++ web/static/multiselect.js
@@ -5,7 +5,7 @@ var multiselect_dict = {};
 var drag_start = false;
 var active_node_id = null;
 
-dr = new Selectables({
+var dr = new Selectables({
     zone:'#drawflow',
     elements: '.title-box',
 
@@ -50,6 +50,48 @@ dr = new Selectables({
 
     enabled: true
 });
+
+var rb = function () {
+    return document.getElementById('s-rectBox');
+};
+
+var cross = function (a, b) {
+    var aTop = offset(a).top, aLeft = offset(a).left, bTop = offset(b).top, bLeft = offset(b).left;
+    return !(((aTop + a.offsetHeight) < (bTop)) || (aTop > (bTop + b.offsetHeight * editor.zoom)) || ((aLeft + a.offsetWidth) < bLeft) || (aLeft > (bLeft + b.offsetWidth * editor.zoom)));
+};
+
+var offset = function (el) {
+    var r = el.getBoundingClientRect();
+    return {top: r.top + document.body.scrollTop, left: r.left + document.body.scrollLeft}
+};
+
+dr.select = function (e) {
+    var a = rb();
+    if (!a) {
+        return;
+    }
+    delete(dr.ipos);
+    document.body.classList.remove('s-noselect');
+    document.body.removeEventListener('mousemove', dr.rectDraw);
+    window.removeEventListener('mouseup', dr.select);
+    var s = dr.options.selectedClass;
+    dr.foreach(dr.items, function (el) {
+        if (cross(a, el) === true) {
+            if (el.classList.contains(s)) {
+                el.classList.remove(s);
+                dr.options.onDeselect && dr.options.onDeselect(el);
+            } else {
+                el.classList.add(s);
+                dr.options.onSelect && dr.options.onSelect(el);
+            }
+        }
+        setTimeout(function () {
+            el.removeEventListener('click', dr.suspend, true);
+        }, 100);
+    });
+    a.parentNode.removeChild(a);
+    dr.options.stop && dr.options.stop(e);
+}
 
 function node_remove_listener(el) {
     let node = el.closest('.drawflow-node');
