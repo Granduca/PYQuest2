e8d4f692e9d50e3bd8e63693bb4a5df3db8b425e
Demian_Scales <demianscales@gmail.com>
2021-12-23 13:15:07 +0300

    update quest object with database object metaclass

--- core/quest.py
+++ core/quest.py
@@ -1,54 +1,81 @@
-from . import Network, Question, Answer
+from typing import List
 from pref import Preferences
 
-import logging
+from core.base import DatabaseObject
+from core.network import Network
 
+from sql.models import Quest as QuestDB
+from sql.models import Network as NetworkDB
+from sql.database import Session
+
+
+import logging
 
 logging.basicConfig(level=Preferences.logging_level_core)
 logger = logging.getLogger(f"{Preferences.app_name} Quest")
 
 
-class Quest:
+class Quest(DatabaseObject):
+    db_object = QuestDB
+
     def __init__(self, title: str):
-        self._title = title
+        self.id = None
+        self.title = title
         self._networks = []
 
-    @property
-    def title(self):
-        return self._title
+    def create_network(self, name: str):
+        network = Network(self.id, name=name)
+        network.save()
+        self._networks.append(network)
+        return network
 
-    @title.setter
-    def title(self, title: str):
-        self._title = title
+    def extend_networks(self, networks: List[NetworkDB]):
+        for network in networks:
+            network = Network(self.id, name=network.name, network_id=network.id)
+            self._networks.append(network)
 
-    def add_network(self):
-        n = Network()
-        self._networks.append(n)
-        return n
+    def get_networks(self):
+        return self._networks
 
-    def add_question(self, **kwargs):
-        text = None
-        if not self._networks:
-            network = self.add_network()
-        else:
-            network = self._networks[0]
-        if 'text' in kwargs:
-            text = kwargs['text']
-        q = Question()
-        q.network = network
-        q.text = text
-        return q
+    def save(self):
+        """Save Quest to database"""
+        save_object = self.db_object(title=self.title)
+        with Session() as session:
+            session.add(save_object)
+            session.commit()
 
-    def add_answer(self, **kwargs):
-        text = None
-        if not self._networks:
-            network = self.add_network()
-        else:
-            network = self._networks[0]
-        if 'text' in kwargs:
-            text = kwargs['text']
-        a = Answer()
-        a.network = network
-        a.text = text
-        return a
+        self.id = save_object.id
 
+    def update(self):
+        update_object = self.db_object(id=self.id, title=self.title)
+        with Session() as session:
+            session.add(update_object)
+            session.commit()
+
+    @classmethod
+    def load(cls, quest_id: int):
+        """Load Quest object from database"""
+        with Session as session:
+            quest_db = session.query(cls.db_object).filter_by(id=quest_id).one()
+            networks = quest_db.networks
+            quest = cls(quest_db.title)
+            quest.id = quest_db.id
+            quest.extend_networks(networks)
+        return quest
+
+
+""" DEBUG """
+if __name__ == "__main__":
+    from sql.database import init_db
+
+    def debug():
+        title = "Первый квест"
+        quest = Quest(title)
+        quest.save()
+
+        quest = Quest.load(quest.id)
+        assert quest.title == title, "Название квеста не соответствует"
+        assert not quest.get_networks(), "У нового квеста существуют сети нод"
+
+    init_db()
+    debug()
