e3d1cd27ec44ff67ba75d9a7e1e57b1a4f109ceb
Demian_Scales <demianscales@gmail.com>
2022-01-01 19:48:19 +0300

    refactor: test_* prefix name change to debug_*

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from core import *
-from tools import tests
+from tools.debug import debug_quest_generator
 
 
 def main():
-    # user = User('')
-    # db = DB(user)
-    # db.create_base_tables()
 
     q = Quest('Test Quest 01')
-    tests.test_quest_generator(q, 200, 4)
+    debug_quest_generator(q, 200, 4)
 
 
 if __name__ == '__main__':
